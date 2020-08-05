891e1b9784154c0b774a48801c014c3eb3911845
Ekam <akashdeep0406@gmail.com>
2020-08-05 08:43:55 +0530

    Minor Error Resolved

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import Modal from 'react-modal';
+import React from "react";
+import Modal from "react-modal";
 
-const OptionModal= (props) =>(
-    <Modal
-        isOpen={!!props.selectedOption}
-        contentLabel="Selected Option"
-        onRequestClose={props.handleSelectedOption}
-        closeTimeoutMS={200}
-        className="modal"
-    >
-        <h3 className="modal__title">
-            Selected Option
-        </h3>
-        <p className="modal__body">
-            {props.selectedOption}
-        </p>
-        <button className="button" onClick={props.handleSelectedOption}>Okay</button>
-    </Modal>
+const OptionModal = (props) => (
+  <Modal
+    isOpen={!!props.selectedOption}
+    contentLabel="Selected Option"
+    onRequestClose={props.handleSelectedOption}
+    closeTimeoutMS={200}
+    className="modal"
+    ariaHideApp={false}
+  >
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && (
+      <p className="modal__body">{props.selectedOption}</p>
+    )}
+    <button className="button" onClick={props.handleSelectedOption}>
+      Okay
+    </button>
+  </Modal>
 );
 
 export default OptionModal;
